075dbabfe9f0ae89204ec4b43dac55478c2e6651
Pinyu <122531000+kiwwichu@users.noreply.github.com>
2026-07-17 03:56:52 +0800

    Update config.js

--- admin-login/config.js
+++ admin-login/config.js
@@ -1,4 +1,4 @@
 window.PINYU_ADMIN_LOGIN = {
   GOOGLE_CLIENT_ID: '852484972991-a14k5ri72tpqffk6bp9358mmvunlsrjm.apps.googleusercontent.com',
-  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxC5hV4id_tMyZqmwCi_TSenYDoeuwR7aEP3nEWNJpTPr22l5hjG5W-_NfejDQDNVyr/exec'
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwZ7ufdjuW4UDgthJ80UxalfDZSV-D5sgGLv4PDMvGRJgfM3xcNcc9yNcw5b4QXSKEkfQ/exec'
 };
