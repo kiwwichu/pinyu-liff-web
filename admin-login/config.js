5f89be974da5bdf2bb306564b46884e04f1262fb
Pinyu <122531000+kiwwichu@users.noreply.github.com>
2026-07-17 03:41:42 +0800

    Create config.js

---
+++ admin-login/config.js
@@ -0,0 +1,4 @@
+window.PINYU_ADMIN_LOGIN = {
+  GOOGLE_CLIENT_ID: '852484972991-a14k5ri72tpqffk6bp9358mmvunlsrjm.apps.googleusercontent.com',
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbw_AVLXXx8eFVSzDe6kP0G2p8AJom2QDeRLnwsgltKnuO3bGLtBmxqZJxwKdsRW34zH/exec'
+};
