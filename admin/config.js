013f2c0d3f65db074cc0be8e367e179a66ba1325
Pinyu <122531000+kiwwichu@users.noreply.github.com>
2026-07-17 02:32:13 +0800

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
 window.ADMIN_CONFIG = {
   LIFF_ID: '2008600362-SelO878Z',
-  API_URL: 'https://script.google.com/macros/s/AKfycbzEbDXmRSOCuF4Ce8aLYa6WBB6CD1O7URCYy3SXKwwuY-VVZfeviBrbuQLYKOFBjQwEmQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyNSL3eupSuXsEZpxVUlUJBdZ9d0KwHAoHzdAgwWHVYyIO00tVMJxdFDgsbzZ4rHJT1UQ/exec'
 };
