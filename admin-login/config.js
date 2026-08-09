23fbbfb5166b43a5a2290e55d21dc678c89a8a2e
Pinyu <122531000+kiwwichu@users.noreply.github.com>
2026-08-09 16:02:01 +0800

    Update APPS_SCRIPT_WEB_APP_URL in config.js

--- admin-login/config.js
+++ admin-login/config.js
@@ -1,4 +1,4 @@
 window.PINYU_ADMIN_LOGIN = {
   GOOGLE_CLIENT_ID: '852484972991-a14k5ri72tpqffk6bp9358mmvunlsrjm.apps.googleusercontent.com',
-  APPS_SCRIPT_WEB_APP_URL:'https://script.google.com/macros/s/AKfycbwwQ79QwHdiESLohGsnyJBPCQfVaO693bqdUfe4DQMRpe39wPRx6bykOnt-DYqpov0LxQ/exec'
+  APPS_SCRIPT_WEB_APP_URL:'https://script.google.com/macros/s/AKfycbw987OW3MlUM41VzJwJpXoBhG7eiiW231cCTmBycbyrt_IW6hJro6HYxMrn9sXqF7_CIw/exec'
 };
